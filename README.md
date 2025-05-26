d083de9c31b0d36118b42b7a18dfdfc1684017e9
voID <info@screenfx.de>
2025-05-26 10:36:11 +0200

    update vcard

--- README.md
+++
@@ -1,3 +0,0 @@
-https://dkoerner.github.io/esri-links/
-
-https://dkoerner.github.io/esri-links/contact-cards/dkoerner/card.html?email=<email>
--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 function createQRCodeImage(url, qrContainer) {
   // Generate the QR code inside the container
   new QRCode(qrContainer, {
-      text: url,
-      width: 128,
-      height: 128,
-      colorDark: "#000000",
-      colorLight: "#ffffff",
-      correctLevel: QRCode.CorrectLevel.H
+    text: url,
+    width: 128,
+    height: 128,
+    colorDark: "#000000",
+    colorLight: "#ffffff",
+    correctLevel: QRCode.CorrectLevel.H
   });
 
   // The QR code library creates a table-based QR code by default.
   // Extract the 'img' element if it exists.
   const qrImage = qrContainer.querySelector('img');
-  
+
   // If an 'img' element is not created, you might be using a library version or configuration
   // that generates a canvas or SVG instead. You need to handle those cases depending on your requirements.
   // For demonstration, let's assume an 'img' is generated.
@@ -22,19 +22,19 @@ function createQRCodeImage(url, qrContainer) {
 }
 
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Create a URLSearchParams object from the current URL
   const params = new URLSearchParams(window.location.search);
-  
+
   // email --------------
   const email = params.get('email');
-  
+
   // Display the parameter value on the page
   if (email !== null) {
-      document.getElementById('email').textContent = `${email}`;
-      document.getElementById('email').href = `mailto:${email}`;
+    document.getElementById('email').textContent = `${email}`;
+    document.getElementById('email').href = `mailto:${email}`;
   } else {
-      document.getElementById('email').textContent = `email not provided`;
+    document.getElementById('email').textContent = `email not provided`;
   }
 
   // view qr code id requested --------------
@@ -43,11 +43,11 @@ document.addEventListener("DOMContentLoaded", function() {
   const qrjsContainer = document.getElementById("qrjs-container");
 
   // generate qr code image
-  const url = `https://dkoerner.github.io/esri-links/contact-cards/dkoerner/card.html?email=${email}`;
+  const url = `https://voidnk.github.io/vcard/card.html?email=${email}`;
   const img = createQRCodeImage(url, qrjsContainer);
 
   // Add an event listener for the 'load' event
-  img.addEventListener('load', function() {
+  img.addEventListener('load', function () {
     qrCodeContainer.innerHTML = ''; // remove all children
     const newImg = document.createElement("img");
     newImg.src = img.src;
@@ -56,11 +56,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
   function togglePortraitQR() {
-    if( portraitContainer.style.display == "none" ) {
+    if (portraitContainer.style.display == "none") {
       // show portrait and hide qr code
       portraitContainer.style.display = "block";
       qrCodeContainer.style.display = "none";
-    }else{
+    } else {
       // hide portrait and show qr code
       portraitContainer.style.display = "none";
       qrCodeContainer.style.display = "block";
